f4b0bad941c360ff219a6c314a418d47a115162b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-06 21:44:01 +0700

    3.2. Tìm kiếm hiển thị suggest (gợi ý)

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -3,9 +3,10 @@ import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug";
 
-// [GET] /search/result
+// [GET] /search/:type
 export const result = async (req: Request, res: Response) => {
   const keyword: string = `${req.query.keyword}`;
+  const type: string = req.params.type;
 
   let newSongs = [];
   
@@ -28,16 +29,43 @@ export const result = async (req: Request, res: Response) => {
           _id: song.singerId
         });
 
-        song["infoSinger"] = infoSinger;
+        // song["infoSinger"] = infoSinger;
+        newSongs.push({
+          id: song.id,
+          title: song.title,
+          avatar: song.avatar,
+          slug: song.slug,
+          like: song.like,
+          infoSinger: {
+            fullName: infoSinger.fullName
+          }
+        });
       }
 
-      newSongs = songs;
+      // newSongs = songs;
     }
   }
 
-  res.render("client/pages/search/result", {
-    pageTitle: `Kết quả: ${keyword}`,
-    keyword: keyword,
-    songs: newSongs
-  });
+  switch (type) {
+    case "result":
+      res.render("client/pages/search/result", {
+        pageTitle: `Kết quả: ${keyword}`,
+        keyword: keyword,
+        songs: newSongs
+      });
+      break;
+    case "suggest":
+      res.json({
+        code: 200,
+        message: "Thành công!",
+        songs: newSongs
+      });
+      break;
+    default:
+      res.json({
+        code: 400,
+        message: "Lỗi!"
+      });
+      break;
+  }
 };
