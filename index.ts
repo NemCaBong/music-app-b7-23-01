4fcd023f8887e35329c0a9e6c82b48853c8ad970
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-08 22:20:17 +0700

    3. Trang Chỉnh sửa bài hát

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import clientRoutes from "./routes/client/index.route";
 import { systemConfig } from "./config/config";
 import path from "path";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
 
 app.use(express.static("public"));
 
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -13,7 +13,6 @@ router.get("/create", controller.create);
 
 router.post(
   "/create",
-  // upload.single("avatar"),
   upload.fields(
     [
       { name: 'avatar', maxCount: 1 },
@@ -22,6 +21,20 @@ router.post(
   ),
   uploadCloud.uploadFields,
   controller.createPost
+);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.fields(
+    [
+      { name: 'avatar', maxCount: 1 },
+      { name: 'audio', maxCount: 1 }
+    ]
+  ),
+  uploadCloud.uploadFields,
+  controller.editPatch
 );
 
 export const songRoutes: Router = router;
